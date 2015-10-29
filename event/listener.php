774a701ad4da01274a5a7f36f02dbac85ae5e4c8
pierrdu <pierre@duhem.com>
2015-10-29 13:52:16 +0100

    modified:   composer.json 	modified:   config/services.yml 	modified:   event/listener.php

--- event/listener.php
+++ event/listener.php
@@ -14,21 +14,31 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class listener implements EventSubscriberInterface
 {
 
+	/* @var \phpbb\user */
 	protected $user;
 
-	public function __construct(\phpbb\user $user)
+	/* @var \phpbb\template\template */
+	protected $template;
+
+	/* @var \phpbb\controller\helper */
+	protected $helper;
+
+
+	public function __construct(\phpbb\user $user, \phpbb\template\template $template)
 	{
 		$this->user = $user;
+		$this->template = $template;
 	}
 
 	static public function getSubscribedEvents()
 	{
 		return array(
-			'core.user_setup'	 => 'load_language_on_viewtopic',
+			'core.user_setup'	=> 'load_language',
+			'core.page_header'	=> 'build_url',
 		);
 	}
 
-	public function load_language_on_viewtopic($event)
+	public function load_language ($event)
 	{
 		$lang_set_ext = $event['lang_set_ext'];
 		$lang_set_ext[] = array(
@@ -37,4 +47,11 @@ class listener implements EventSubscriberInterface
 		);
 		$event['lang_set_ext'] = $lang_set_ext;
 	} 
+	
+	public function build_url ($event)
+	{
+		$params  = "author=" . $this->user->data['username'] . "&sf=firstpost&sr=topics";
+		$url = append_sid ("./search.php", $params);
+		$this->template->assign_var('U_MYTOPICS', $url);
+	}
 }
