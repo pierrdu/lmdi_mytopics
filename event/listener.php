dd23eb35f9484f592ed2e67dc001842a0a9194a8
pierrdu <pierre@duhem.com>
2017-01-12 14:19:29 +0100

    1.0.7

--- event/listener.php
+++ event/listener.php
@@ -20,11 +20,19 @@ class listener implements EventSubscriberInterface
 	protected $template;
 	protected $root_path;
 	protected $php_ext;
+	/* @var \phpbb\config\config */
+	protected $config;
 
 
-	public function __construct(\phpbb\user $user, \phpbb\template\template $template, $root_path, $phpEx)
+	public function __construct(
+		\phpbb\user $user,
+		\phpbb\config\config $config,
+		\phpbb\template\template $template, 
+		$root_path, 
+		$phpEx)
 	{
 		$this->user = $user;
+		$this->config = $config;
 		$this->template = $template;
 		$this->root_path = $root_path;
 		$this->phpEx = $phpEx;
@@ -50,8 +58,20 @@ class listener implements EventSubscriberInterface
 
 	public function build_url($event)
 	{
+		if (version_compare ($this->config['version'], '3.2.x', '<'))
+		{
+			$mytop_class = 0;
+		}
+		else
+		{
+			$mytop_class = 1;
+		}
+		var_dump ($mytop_class);
 		$params  = "author=" . $this->user->data['username'] . "&amp;sf=firstpost&amp;sr=topics";
 		$url = append_sid($this->root_path . "search." . $this->phpEx, $params);
-		$this->template->assign_var('U_MYTOPICS', $url);
+		$this->template->assign_vars(array(
+			'U_MYTOPICS'	=> $url,
+			'S_320'		=> $mytop_class,
+		));
 	}
 }
