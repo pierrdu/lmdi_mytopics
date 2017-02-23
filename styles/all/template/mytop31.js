2bebdc2480245b066cef9bdc46af6632bf3af9a0
pierrdu <pierre@duhem.com>
2017-02-23 12:09:09 +0100

    1.0.9b

--- styles/all/template/mytop31.js
+++ styles/all/template/mytop31.js
@@ -2,7 +2,7 @@
 $(".icon-search-self").remove();
 // Cela ne peut marcher que pour 3.1.x
 // Voir aussi https://www.phpbb.com/community/viewtopic.php?f=466&t=2398206
-	$(".dropdown-contents").first-child.remove();
+// 	$(".dropdown-contents").first-child.remove();
 //	});
 // });
 // $( ".icon-search-active, .icon-search" ).next().remove();
